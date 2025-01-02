f48e1262a2d1f17fee38f2957aca47a377ad83c8
missymogara <monicagara97@gmail.com>
2025-01-02 10:40:17 +0100

    auth api key left

--- controllers/series.js
+++ controllers/series.js
@@ -1,5 +1,6 @@
 const Series = require("../models/series");
 
+// Create a new serie
 async function createSerie(req, res) {
     console.log("Creando serie");
     const serie = new Series();
@@ -19,7 +20,7 @@ async function createSerie(req, res) {
         const serieStore = await serie.save();
 
         if(!serieStore){
-            res.status(400).send({
+            res.status(404).send({
                 message: "No se pudo guardar la serie."
             });
         } else {
@@ -32,6 +33,130 @@ async function createSerie(req, res) {
     }
 }
 
+// Get all series
+async function getSeries(req, res) {
+    try {
+        const series = await Series.find();
+
+        if(!series) {
+            res.status(404).send({
+                message: "Error al obtener las series."
+            });
+        } else {
+            res.status(200).send(series);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+// Get all series ordered by score
+async function getSeriesOrderedByScore(req, res) {
+    try {
+        const series = await Series.find().sort({ puntuacionTotal: -1 }).limit(10);
+
+        if(!series) {
+            res.status(404).send({
+                message: "Error al obtener las series."
+            });
+        } else {
+            res.status(200).send(series);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+// Get series by category
+async function getSeriesByGenre(req, res) {
+    try {
+        const { genero } = req.params;
+
+        const series = await Series.find({ genero: genero });
+
+        if(!series) {
+            res.status(404).send({
+                message: `Error al obtener las series del genero ${genero}`
+            });
+        } else {
+            res.status(200).send(series);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+// Get a single serie by ID
+async function getSerieByID(req, res) {
+    const serieID = req.params.id;
+
+    try {
+
+        const serie = await Series.findById(serieID);  
+
+        if(!serie) {
+            res.status(404).send({
+                message: "No se encontró la serie."
+            });
+        } else {
+            res.status(200).send(serie);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+// Add more score to a single serie
+async function addScore(req, res) {
+    const serieID = req.params.id;
+    const newScore = req.body.puntuacionTotal;
+    
+    try {
+        const serie = await Series.findByIdAndUpdate(serieID, { $set: { puntuacionTotal: newScore } });
+
+        if(!serie) {
+            res.status(404).send({
+                message: "No se encontró la serie."
+            });
+        } else {
+            res.status(200).send({
+                message: "Puntaje añadido correctamente."
+            });
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+}
+
+// Delete a serie from the database
+async function deleteSerie(req, res) {
+    const serieID = req.params.id;
+    
+    try {
+        const serie = await Series.findByIdAndDelete(serieID);
+
+        if(!serie) {
+            res.status(404).send({
+                message: "No se encontró la serie."
+            });
+        } else {
+            res.status(200).send({
+                message: "Serie eliminada correctamente."
+            });
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
 module.exports = {
     createSerie,
+    getSeries,
+    getSeriesOrderedByScore,
+    getSeriesByGenre,
+    getSerieByID,
+    addScore,
+    deleteSerie,
 };
+
--- routes/series.js
+++ routes/series.js
@@ -4,5 +4,11 @@ const seriesController = require('../controllers/series');
 const api = express.Router();
 
 api.post("/series", seriesController.createSerie);
+api.get("/series", seriesController.getSeries);
+api.get("/series/toprated", seriesController.getSeriesOrderedByScore);
+api.get('/series/genre/:genero', seriesController.getSeriesByGenre);
+api.get('/series/id/:id', seriesController.getSerieByID);
+api.put('/series/:id', seriesController.addScore);
+api.delete('/series/:id', seriesController.deleteSerie);
 
 module.exports = api;
